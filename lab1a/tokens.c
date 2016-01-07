eeb1b4a9b56d0c4f4227f502c6d1f0597dd36cd6
eltonkl <eltonkl@users.noreply.github.com>
2016-01-06 22:32:40 -0800

    Formatting

--- lab1a/tokens.c
+++ lab1a/tokens.c
@@ -40,20 +40,29 @@ token_tuple_t TOKENS[24] =
 bool validate_option(option_t* opt)
 {
 	//If none of the options are chosen then we don't have a valid option
-	if (opt->type < 0 || opt->type > 4) return false;
+	if (opt->type < 0 || opt->type > 4)
+            return false;
 	
 	//Otherwise, we must check to make sure that the option has the appropriate number of arguments
-	if(opt->type == file_flag_ && opt->num_args != 0) return false;									//All flags have no args
-	if(opt->type == file_open_)																		//All opens have one arg, except pipe which has no args
+	if(opt->type == file_flag_ && opt->num_args != 0)
+            return false;
+        //All flags have no args
+	if(opt->type == file_open_)
+            //All opens have one arg, except pipe which has no args
 	{
-		if((opt->option == rdwr_ || opt->option == rdonly_ || opt->option == wronly_) && opt->num_args != 1) return false;
-		if(opt->option == pipe_ && opt->num_args != 0) return false;
+		if((opt->option == rdwr_ || opt->option == rdonly_ || opt->option == wronly_) && opt->num_args != 1) 
+                    return false;
+		if(opt->option == pipe_ && opt->num_args != 0)
+                    return false;
 	}
 	
-	if(opt->type == subcommand_)																	//command has i e o cmd and 0 or more args, wait has no args
+	if(opt->type == subcommand_)
+            //command has i e o cmd and 0 or more args, wait has no args
 	{
-		if(opt->option == command_ && opt->num_args < 4) return false;
-		if(opt->option == wait_ && opt->num_args != 0) return false;
+		if(opt->option == command_ && opt->num_args < 4) 
+                    return false;
+		if(opt->option == wait_ && opt->num_args != 0)
+                    return false;
 	}
 	
 	if(opt->type == misc_)																			//Some misc have no args, some have one arg
@@ -66,3 +75,16 @@ bool validate_option(option_t* opt)
 	//If we make it this far, then our option is indeed valid.
 	return true;
 }
+
+//Input: num_args > 0 
+//Input: args[0] contains an option 
+option_t* create_flag(int* num_args, char** args) 
+{ 
+    option_t* opt = malloc(sizeof(option_t)); 
+    if (!opt) 
+        return NULL; 
+    for (int i = 0; i < sizeof(TOKENS)/sizeof(token_tuple_t); i++) 
+    { 
+        //if (strcmp(args[0], TOKENS[i] 
+    } 
+} 
