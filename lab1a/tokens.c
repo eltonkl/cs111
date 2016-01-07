1a311eedf7023dff5966068473017a58b2c8274b
jameswang27 <timmyorjames42@gmail.com>
2016-01-06 22:16:18 -0800

    Fixed errors in validate_option

--- lab1a/tokens.c
+++ lab1a/tokens.c
@@ -6,23 +6,23 @@ bool validate_option(option_t* opt)
 	if (opt->type < 0 || opt->type > 4) return false;
 	
 	//Otherwise, we must check to make sure that the option has the appropriate number of arguments
-	if(opt->type == file_flag_ && num_args != 0) return false;										//All flags have no args
+	if(opt->type == file_flag_ && opt->num_args != 0) return false;									//All flags have no args
 	if(opt->type == file_open_)																		//All opens have one arg, except pipe which has no args
 	{
-		if((option == rdwr_ || option == rdonly_ || option == wronly_) && num_args != 1) return false;
-		if(option == pipe_ && num_args != 0) return false;
+		if((opt->option == rdwr_ || opt->option == rdonly_ || opt->option == wronly_) && opt->num_args != 1) return false;
+		if(opt->option == pipe_ && opt->num_args != 0) return false;
 	}
 	
 	if(opt->type == subcommand_)																	//command has i e o cmd and 0 or more args, wait has no args
 	{
-		if(option == command_ && num_args < 4) return false;
-		if(option == wait_ && num_args != 0) return false;
+		if(opt->option == command_ && opt->num_args < 4) return false;
+		if(opt->option == wait_ && opt->num_args != 0) return false;
 	}
 	
 	if(opt->type == misc_)																			//Some misc have no args, some have one arg
 	{
-		if ((option == catch_ || option == ignore_ || option == default_) && num_args != 1) return false;
-		if ((option == verbose_ || option == profile_ || option == abort_ || option == pause_) && num_args != 0) return false;
+		if ((opt->option == catch_ || opt->option == ignore_ || opt->option == default_) && opt->num_args != 1) return false;
+		if ((opt->option == verbose_ || opt->option == profile_ || opt->option == abort_ || opt->option == pause_) && opt->num_args != 0) return false;
 	}
 	
 	
