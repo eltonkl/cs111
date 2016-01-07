08fc7a904e45ca0aec4ab97de7f102c771b8da7b
eltonkl <eltonkl@users.noreply.github.com>
2016-01-06 21:52:08 -0800

    Add token pairs

--- lab1a/tokens.h
+++ lab1a/tokens.h
@@ -3,54 +3,102 @@
 
 #include <stdbool.h>
 
+#define TOKEN_PAIR(x) { x##_, "--##x" }
+
+typedef struct token_pair_t
+{
+    int option;
+    char* value;
+} token_pair_t;
+
 typedef enum token_t
 {
-    file_flag,
-    file_open,
-    subcommand,
-    misc,
-    unexpected
+    file_flag_ = 0,
+    file_open_,
+    subcommand_,
+    misc_,
+    unexpected_
 } token_t;
 
 typedef enum file_flag_options_t
 {
-    append,
-    cloexec,
-    creat,
-    directory,
-    dsync,
-    excl,
-    nofollow,
-    nonblock,
-    rsync,
-    sync,
-    trunc
+    append_ = 0,
+    cloexec_,
+    creat_,
+    directory_,
+    dsync_,
+    excl_,
+    nofollow_,
+    nonblock_,
+    rsync_,
+    sync_,
+    trunc_
 } file_flag_option_t;
+
+token_pair_t FILE_FLAG_OPTION_TOKENS[] = 
+{
+    TOKEN_PAIR(append),
+    TOKEN_PAIR(cloexec),
+    TOKEN_PAIR(creat),
+    TOKEN_PAIR(directory),
+    TOKEN_PAIR(dsync),
+    TOKEN_PAIR(excl),
+    TOKEN_PAIR(nofollow),
+    TOKEN_PAIR(nonblock),
+    TOKEN_PAIR(rsync),
+    TOKEN_PAIR(sync),
+    TOKEN_PAIR(trunc)
+};
 
 typedef enum file_open_options_t
 {
-    rdonly,
-    rdwr,
-    wronly,
-    pipe
+    rdonly_ = 0,
+    rdwr_,
+    wronly_,
+    pipe_
 } file_open_options_t;
+
+token_pair_t FILE_OPEN_OPTION_TOKENS[] =
+{
+    TOKEN_PAIR(rdonly),
+    TOKEN_PAIR(rdwr),
+    TOKEN_PAIR(wronly),
+    TOKEN_PAIR(pipe)
+};
 
 typedef enum subcommand_options_t
 {
-    command,
-    wait
+    command_ = 0,
+    wait_
 } subcommands_options_t;
+
+token_pair_t SUBCOMMAND_OPTION_TOKENS[] =
+{
+    TOKEN_PAIR(command),
+    TOKEN_PAIR(wait)
+};
 
 typedef enum misc_options_t
 {
-    verbose,
-    profile,
+    verbose_ = 0,
+    profile_,
     abort_,
     catch_,
-    ignore,
+    ignore_,
     default_,
-    pause
+    pause_
 } misc_options_t;
+
+token_pair_t MISC_OPTION_TOKENS[] =
+{
+    TOKEN_PAIR(verbose),
+    TOKEN_PAIR(profile),
+    TOKEN_PAIR(abort),
+    TOKEN_PAIR(catch),
+    TOKEN_PAIR(ignore),
+    TOKEN_PAIR(default),
+    TOKEN_PAIR(pause)
+};
 
 typedef struct option_t
 {
@@ -60,6 +108,6 @@ typedef struct option_t
     char** args;
 } option_t;
 
-bool validate_option(option_t* f);
+bool is_valid_option(option_t* opt);
 
 #endif
