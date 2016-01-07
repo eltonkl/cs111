88a8952114dbb097d3a91b0387a4d5a6234e819e
jameswang27 <timmyorjames42@gmail.com>
2016-01-06 22:08:12 -0800

    validate_option implementation

---
+++ lab1a/tokens.c
@@ -0,0 +1,31 @@
+#include "tokens.h"
+
+bool validate_option(option_t* opt)
+{
+	//If none of the options are chosen then we don't have a valid option
+	if (opt->type < 0 || opt->type > 4) return false;
+	
+	//Otherwise, we must check to make sure that the option has the appropriate number of arguments
+	if(opt->type == file_flag_ && num_args != 0) return false;										//All flags have no args
+	if(opt->type == file_open_)																		//All opens have one arg, except pipe which has no args
+	{
+		if((option == rdwr_ || option == rdonly_ || option == wronly_) && num_args != 1) return false;
+		if(option == pipe_ && num_args != 0) return false;
+	}
+	
+	if(opt->type == subcommand_)																	//command has i e o cmd and 0 or more args, wait has no args
+	{
+		if(option == command_ && num_args < 4) return false;
+		if(option == wait_ && num_args != 0) return false;
+	}
+	
+	if(opt->type == misc_)																			//Some misc have no args, some have one arg
+	{
+		if ((option == catch_ || option == ignore_ || option == default_) && num_args != 1) return false;
+		if ((option == verbose_ || option == profile_ || option == abort_ || option == pause_) && num_args != 0) return false;
+	}
+	
+	
+	//If we make it this far, then our option is indeed valid.
+	return true;
+}
