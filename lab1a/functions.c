dfa4652c2160fa02cc7690cca8a5a4131c48cb67
jameswang27 <timmyorjames42@gmail.com>
2016-01-07 17:56:28 -0800

    Basic changes, disregard.

--- lab1a/functions.c
+++ lab1a/functions.c
@@ -1,2 +1,15 @@
-
+#include <unistd.h>                                                 //close, fork, getopt_long, pipe, dup2, execvp
+#include <sys/types.h> #include <sys/stat.h> #include <fcntl.h>     //open and creat
 #include "functions.h"
+
+int rdonly(const char* file)
+{
+    int fd = open(file, O_RDONLY);
+    return fd;
+}
+
+int wronly(const char* file)
+{
+    int fd = open(file, O_WRONLY);
+    return fd;
+}
--- lab1a/functions.h
+++ lab1a/functions.h
@@ -1,12 +1,10 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-#include <unistd.h>
-
 //Function for rdonly
-
+int rdonly(const char* file);
 //Function for wronly
-
+int wronly(const char* file);
 //Function for command
 
 //Function for verbose
