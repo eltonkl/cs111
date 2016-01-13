6fa1131ff6d61c697cc8d783f92634b1598cffa6
jameswang27 <timmyorjames42@gmail.com>
2016-01-12 19:38:16 -0800

    More test cases, commented test cases for clarity

--- lab1a/test2.sh
+++ lab1a/test2.sh
@@ -22,6 +22,7 @@ errors=/testfiles/errors
 #TESTING
 #The general format of the segment is to run a possible simpsh use and then examine the output to see if it appears to be the proper output
 
+#File opening
 ./simpsh --rdonly nosuchfile
 test(1) "Report error if no file is available for reading only"
 
@@ -34,6 +35,7 @@ test(0) "Test case where the file exists for reading only and opens correctly"
 ./simpsh --wronly $t1
 test(0) "Test case where the file exists for writing only and opens correctly"
 
+#Command call validation
 ./simpsh --command 1 2 3 echo $t1
 test(1) "Report error if command attempts to use an out of range file descriptor"
 
@@ -43,11 +45,17 @@ test(1) "Report error if command uses the wrong number of options"
 ./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 0 1 2 cat $t1 $t2
 test(0) "Test case where we open a file for reading, a file for writing, and a file for error writing and execute the cat command."
 
+#Inappropriate action catches
 ./simpsh --rdonly $t1 --rdonly $t2 --wronly $t3 --command 0 1 2 cat $t1 $t2
 test(1) "Report error if command attempts to write to a read only file"
 
 ./simpsh --wronly $t1 --rdonly $t2 --wronly $t3 --command 0 1 2 cat $t1 $t2
 test(1) "Report error if command attempts to read from a write only file"
 
+#Command warnings NOTE: not sure exactly what the exit status of these commands should be. Please advise.
+./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 1 0 2 cat $t1 $t2
+test(0) "Report warning if trying to use rdonly file in stdout and trying to use wronly file in stdin, without interuppting execution"
 
+./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 2 1 0 cat $t1 $t2
+test(0) "Report warning if trying to use rdonly file in stderr, without interuppting execution"
 
