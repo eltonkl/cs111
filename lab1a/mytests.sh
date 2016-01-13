929277d8a8cd34ab4a4c88c82f3093b36b61be5c
James Wang <jameswa@lnxsrv05.seas.ucla.edu>
2016-01-12 21:14:49 -0800

    Fixed test cases syntax

---
+++ lab1a/mytests.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#a simple function that will print out the appropriate prefix in the message after each test case has been executed
+#the input argument is the value that the previous execution should have ended up with based on the test case
+function testrun
+{
+    result=$?;
+    if [ $result != $1 ]; then
+        echo "TEST FAILED: ";
+        echo;
+    else
+        echo "TEST SUCCEEDED: ";
+        echo;
+    fi
+}
+
+#create simpler associations to the test files for the purposes of testing. 
+t1=/testfiles/foo
+t2=/testfiles/bar
+errors=/testfiles/errors
+
+#TESTING
+#The general format of the segment is to run a possible simpsh use and then examine the output to see if it appears to be the proper output
+
+#File opening
+./simpsh --rdonly nosuchfile
+testrun 1 "Report error if no file is available for reading only"
+
+./simpsh --wronly nosuchfile
+testrun 1 "Report error if no file is available for writing only"
+
+./simpsh --rdonly $t1
+testrun 0 "Test case where the file exists for reading only and opens correctly"
+
+./simpsh --wronly $t1
+testrun 0 "Test case where the file exists for writing only and opens correctly"
+
+#Command call validation
+./simpsh --command 1 2 3 echo $t1
+testrun 1 "Report error if command attempts to use an out of range file descriptor"
+
+./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 1 2 3
+testrun 1 "Report error if command uses the wrong number of options"
+
+./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 0 1 2 cat $t1 $t2
+testrun 0 "Test case where we open a file for reading, a file for writing, and a file for error writing and execute the cat command."
+
+#Inappropriate action catches
+./simpsh --rdonly $t1 --rdonly $t2 --wronly $t3 --command 0 1 2 cat $t1 $t2
+testrun 1 "Report error if command attempts to write to a read only file"
+
+./simpsh --wronly $t1 --rdonly $t2 --wronly $t3 --command 0 1 2 cat $t1 $t2
+testrun 1 "Report error if command attempts to read from a write only file"
+
+#Command warnings NOTE: not sure exactly what the exit status of these commands should be. Please advise.
+./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 1 0 2 cat $t1 $t2
+testrun 0 "Report warning if trying to use rdonly file in stdout and trying to use wronly file in stdin, without interuppting execution"
+
+./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 2 1 0 cat $t1 $t2
+testrun 0 "Report warning if trying to use rdonly file in stderr, without interupting execution"
+
+#Continue execution even after a failed command
+./simpsh --verbose --wronly $t1 --command 1 2 3 echo foo --command 0 0 0 echo foo
+testrun 0 "Continue execution even after a failed command"
--- lab1a/test.sh
+++ lab1a/test.sh
@@ -1,124 +1,64 @@
 #!/bin/bash
 
-function should_fail() {
-  result=$?;
-
-  echo -n "==> $1 ";
-
-  if [ $result -lt 1 ]; then
-    echo "FAILURE";
-    exit 1;
-  else
-    echo;
-  fi
+#a simple function that will print out the appropriate prefix in the message after each test case has been executed
+#the input argument is the value that the previous execution should have ended up with based on the test case
+function test $arg1
+{
+    result=$?;
+    if [ $result != $1 ]; then
+        echo "TEST FAILED: ";
+        echo;
+    else
+        echo "TEST SUCCEEDED: ";
+        echo;
+    fi
 }
 
-function should_succeed() {
-  result=$?;
+#create simpler associations to the test files for the purposes of testing. 
+t1=/testfiles/foo
+t2=/testfiles/bar
+errors=/testfiles/errors
 
-  echo -n "==> $1 ";
+#TESTING
+#The general format of the segment is to run a possible simpsh use and then examine the output to see if it appears to be the proper output
 
-  if [ $result -gt 0 ]; then
-    echo "FAILURE";
-    exit 1;
-  else
-    echo;
-  fi
-}
+#File opening
+./simpsh --rdonly nosuchfile
+test(1) "Report error if no file is available for reading only"
 
-tmp_file=/tmp/foo
-tmp_file2=/tmp/foo2
-> "$tmp_file"
-> "$tmp_file2"
+./simpsh --wronly nosuchfile
+test(1) "Report error if no file is available for writing only"
 
+./simpsh --rdonly $t1
+test(0) "Test case where the file exists for reading only and opens correctly"
 
-./simpsh --rdonly cantpossiblyexist 2>&1 | grep "Failed to open read" > /dev/null
-should_succeed "reports missing file";
+./simpsh --wronly $t1
+test(0) "Test case where the file exists for writing only and opens correctly"
 
+#Command call validation
+./simpsh --command 1 2 3 echo $t1
+test(1) "Report error if command attempts to use an out of range file descriptor"
 
-./simpsh --rdonly Makefile | grep "Failed to open read" > /dev/null;
-should_fail "does not report file that exists"
+./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 1 2 3
+test(1) "Report error if command uses the wrong number of options"
 
+./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 0 1 2 cat $t1 $t2
+test(0) "Test case where we open a file for reading, a file for writing, and a file for error writing and execute the cat command."
 
-./simpsh --verbose --command 1 2 3 echo foo 2>&1 | grep "not a valid" > /dev/null
-should_succeed "using a non existent file descriptor should report the error"
+#Inappropriate action catches
+./simpsh --rdonly $t1 --rdonly $t2 --wronly $t3 --command 0 1 2 cat $t1 $t2
+test(1) "Report error if command attempts to write to a read only file"
 
+./simpsh --wronly $t1 --rdonly $t2 --wronly $t3 --command 0 1 2 cat $t1 $t2
+test(1) "Report error if command attempts to read from a write only file"
 
-> "$tmp_file"
-(./simpsh \
-  --verbose \
-  --wronly "$tmp_file" \
-  --command 1 2 3 echo foo 2>&1 \
-  --command 0 0 0 echo foo ) 2>/dev/null 1>/dev/null
+#Command warnings NOTE: not sure exactly what the exit status of these commands should be. Please advise.
+./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 1 0 2 cat $t1 $t2
+test(0) "Report warning if trying to use rdonly file in stdout and trying to use wronly file in stdin, without interuppting execution"
 
-grep "foo" "$tmp_file" > /dev/null
-should_succeed "commands after failing commands should succeed"
+./simpsh --rdonly $t1 --wronly $t2 --wronly $t3 --command 2 1 0 cat $t1 $t2
+test(0) "Report warning if trying to use rdonly file in stderr, without interupting execution"
 
-
-./simpsh --verbose --command 2>&1 | grep "command requires an argument" > /dev/null
-should_fail "empty command should have no options"
-
-
-./simpsh --verbose --command 1 2 3 2>&1 | grep "invalid number of args" > /dev/null
-should_succeed "command requires at least 4 arguments"
-
-
-./simpsh --verbose --command 1 2 3 2>&1 | grep "invalid number of args" > /dev/null
-should_succeed "command reports malformed command options to user on stderr"
-
-
-./simpsh \
-  --wronly /tmp/foo \
-  --verbose \
-  --command 0 0 0 echo foo \
-  | grep "command 0 0 0 echo foo" > /dev/null
-should_succeed "command tracks all command options"
-
-
-./simpsh --wronly /tmp/foo --command 0 0 0 echo "foo";
-grep foo /tmp/foo > /dev/null;
-should_succeed "command can write to write only file"
-
-
-> "$tmp_file"
-echo_path=$(which echo);
-./simpsh --wronly /tmp/foo --command 0 0 0 "$echo_path" "foo";
-grep foo /tmp/foo > /dev/null;
-should_succeed "path command can write to write only file"
-
-> "$tmp_file"
-./simpsh --rdonly /tmp/foo --command 0 0 0 echo "foo";
-grep foo /tmp/foo > /dev/null;
-# NOTE that failure of `echo "foo"` end up in stderr
-should_fail "shouldn't be able to write to read only file"
-
-
-echo "foo" > "$tmp_file"
-cat "$tmp_file" | wc -l | grep 1 > /dev/null
-should_succeed "the temporary file should have one line"
-# the cat of $tmp_file should be empty and not add another line to tmp_file
-./simpsh --wronly "$tmp_file" --command 0 0 0 cat "$tmp_file"
-cat "$tmp_file" | wc -l | grep 1 > /dev/null
-should_succeed "shouldn't be able to write to read only file"
-
-
-echo "foo" > "$tmp_file"
-echo "bar" > "$tmp_file2"
-
-cat "$tmp_file" | grep "foo" > /dev/null
-should_succeed "the temporary file should have 'foo'"
-
-cat "$tmp_file2" | grep "bar" > /dev/null
-should_succeed "the temporary file 2 should have 'bar'"
-
-# cat of /tmp/foo should end up in the /tmp/file2
-./simpsh --rdonly "$tmp_file" --wronly "$tmp_file2" --command 0 1 0 cat "$tmp_file"
-cat "$tmp_file2" | grep "foo" > /dev/null && cat "$tmp_file2" | wc -l | grep 1 > /dev/null
-should_succeed "should be able to cat from one file to the other (replace bar with foo)"
-
-
-./simpsh --rdonly "$tmp_file" --command 0 0 0 echo "foo"
-echo "$?" | grep "1" > /dev/null
-should_succeed "exit status of failed subcommand should be the exit status of simpsh"
-
-echo "Success"
+#Continue execution even after a failed command
+./simpsh --verbose --wronly $t1 --command 1 2 3 echo foo --command 0 0 0 echo foo
+test(0) "Continute execution even after a failed command"
