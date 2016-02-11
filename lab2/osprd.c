a71dc932000fc7176022d44060da2407494b519b
James Wang <jameswang97@ucla.edu>
2016-02-10 21:45:29 -0800

    Changed kernel author names to our names

--- lab2/osprd.c
+++ lab2/osprd.c
@@ -34,7 +34,7 @@
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("CS 111 RAM Disk");
 // EXERCISE: Pass your names into the kernel as the module's authors.
-MODULE_AUTHOR("Skeletor");
+MODULE_AUTHOR("Elton Leong and James Wang");
 
 #define OSPRD_MAJOR	222
 
